2c5c099f184857e3e9562fbd876ef4008f3c06f9
simonzhangs <2863389578@qq.com>
2023-04-14 18:08:37 +0800

    remove: bundle-analyzer

--- vue.config.js
+++ vue.config.js
@@ -71,7 +71,7 @@ module.exports = {
     }
 
     // 打包体积分析
-    config.plugin('webpack-bundle-analyzer')
-          .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+    // config.plugin('webpack-bundle-analyzer')
+    //       .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
   },
 };
